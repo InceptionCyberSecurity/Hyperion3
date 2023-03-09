93042a94f48d30585c083e0ae702353b0c01f0f3
Arcadeus OPS <info@arcadeusops.com>
2023-03-09 17:27:44 +0800

    Update back.sh

--- back.sh
+++ back.sh
@@ -5,26 +5,26 @@
 # Second Argument $2: $udir directory to store reports
 # User Input from  command line arguments
 userIP="$1" # IP address eg 8.8.8.8
-udir="$2" # directory for reports 
+udir="$2" # directory for reports
 
 # shellshock
-sudo nmap -sV -p- --script http-shellshock $userIP -oX shell.xml
+sudo nmap -vv -sV -p- --script http-shellshock $userIP -oX shell.xml
 xsltproc shell.xml -o shellshock.html
 
 # targeted shellshock
-sudo nmap -sV -p- --script http-shellshock --script-args uri=/cgi-bin/bin,cmd=ls $userIP -oX shell1.xml
+sudo nmap -vv -sV -p- --script http-shellshock --script-args uri=/cgi-bin/bin,cmd=ls $userIP -oX shell1.xml
 xsltproc shell1.xml -o shellshock1.html
 
 # heartbleed
-sudo nmap -p 443 --script ssl-heartbleed $userIP -oX hb.xml
+sudo nmap -vv -p 443 --script ssl-heartbleed $userIP -oX hb.xml
 xsltproc hb.xml -o heartbleed.html
 
 # VNC
-sudo nmap -sV -sC --script vnc* -p 5900 --script=vnc-title $userIP -oX v.xml
+sudo nmap -vv -sV -sC --script vnc* -p 5900 --script=vnc-title $userIP -oX v.xml
 xsltproc v.xml -o vnc.html
 
 # CITRIX
-sudo nmap -sU --script citrix* -p - $userIP -oX ctx.xml
+sudo nmap -vv -sU --script citrix* -p - $userIP -oX ctx.xml
 xsltproc ctx.xml -o citrix.html
 
 # local storage ready for upload to client's container
